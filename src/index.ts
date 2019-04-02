77952ce70b2aec0be6a0c19a725c72159edc8f63
rymdo <rymdo.sl@gmail.com>
2019-04-02 20:45:13 +0200

    Run type-check, build and test on pre-commit and skip node_modules on tsc

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,15 @@
 export {};
 
-//
+// tslint:disable:no-console
+console.log('Running....');
+
+const testFunc = () => {
+  return new Promise((resolve, reject) => {
+    resolve();
+  });
+};
+
+testFunc().then(() => {
+  // tslint:disable:no-console
+  console.log('YAY');
+});
