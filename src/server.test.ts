d0e2f00d28b7917cdbc0f103fbc8d33d5c068df2
rymdo <rymdo.sl@gmail.com>
2019-04-08 17:48:46 +0200

    Refactored server test

--- src/server.test.ts
+++ src/server.test.ts
@@ -1,24 +1,25 @@
 import { Server } from './server';
 describe('server', () => {
-  let server: Server;
-
-  beforeEach(async () => {
-    server = new Server();
-    await server.start();
-  });
-
-  afterEach(async () => {
-    try {
+  describe('when started', () => {
+    let server: Server;
+    beforeEach(async () => {
+      server = new Server();
+      await server.start();
+    });
+    afterEach(async () => {
       await server.stop();
-    } catch (_) {
-      // Ignore any catch
-    }
+    });
+    describe('should trow', () => {
+      it('if already started', async () => {
+        await expect(server.start()).rejects.toEqual('server already started');
+      });
+    });
   });
-  it('should throw when starting already started server', async () => {
-    await expect(server.start()).rejects.toEqual('server already started');
-  });
-  it('should throw when stopping not started server', async () => {
-    await server.stop(); // Stop server before next line
-    await expect(server.stop()).rejects.toEqual('server not started');
+
+  describe('should throw', () => {
+    it('if calling stop while not started', async () => {
+      const server = new Server();
+      await expect(server.stop()).rejects.toEqual('server not started');
+    });
   });
 });
